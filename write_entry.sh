74832542158ce2dddc6fc1353a4670dc77b9d66e
Gerard Altamirano <hello@itsgerard.com>
2024-02-21 05:27:50 +0100

    Fix INSERT query in write_entry script
    - Fix post content not being treated as string when inserting into the
      DB
    - Do not force HTTPS in fly.toml config file

--- write_entry.sh
+++ write_entry.sh
@@ -7,9 +7,12 @@ vim "$file"
 # Check if file exists
 if [ -f "$file" ]; then
     title=$(awk 'NR==1' $file | sed 's!^[\t\s+#]*!!')
+    # TODO: Extract subtract of entry to use in index
+    # TODO: Add support for tags
+    # TODO: Add support for more metadata (covers, tags, links, etc)
     content="$(< $file)"
     
-    sqlite3 -line db.sqlite3 "INSERT INTO post VALUES(null, '$title', '$content', datetime('now'));"
+    sqlite3 -line db.sqlite3 "INSERT INTO post VALUES(null,\"$title\", \"$content\",  datetime('now'));"
     rm "$file"
 fi
 
