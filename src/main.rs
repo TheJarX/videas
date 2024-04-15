ebec13c863ba6dd9cf7f90a15642ec635812c227
Gerard Altamirano <hello@itsgerard.com>
2024-04-15 04:47:53 +0200

    Add comment about dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use actix_web::{App, HttpServer, middleware::Logger};
 use actix_files as fs;
+use actix_web::{middleware::Logger, App, HttpServer};
 use dotenvy::dotenv;
 use videas::routes::*;
 
 #[actix_web::main]
 #[doc(hidden)]
 async fn main() -> std::io::Result<()> {
-    dotenv().expect(".env file not found");
+    // We need this to load the .env file  only in development.
+    // It's better to use other ways to load the environment variables in production.
+    dotenv().ok();
     env_logger::init();
 
     HttpServer::new(|| {
@@ -21,4 +23,3 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
-
